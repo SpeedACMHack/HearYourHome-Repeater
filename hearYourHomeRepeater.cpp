30910d129baadf5e9c749503998be8138c5b2bfc
AwesomeShayne <shayne.hemminger@louisville.edu>
2016-04-16 21:26:35 -0400

    Began the arduino file

---
+++ hearYourHomeRepeater.cpp
@@ -0,0 +1,23 @@
+/*
+Purpose:    This program is designed to listen for LISNR Signals and repeat them if necessary
+Developer:  Shayne Hemminger, Tyler Ault, Jacob Berdichevsky
+*/
+
+
+// This next line is important if you are running this code on a photon rather than an arduino.
+// This defeats the purpose of using this project. But can be used for testing purposes.
+// #include 'application.h' 
+
+
+// Initialize Variables
+
+
+// Set pin purposes
+void setup() {
+    
+}
+
+// This code executes constantly while the device is on.
+void loop() {
+    
+}
